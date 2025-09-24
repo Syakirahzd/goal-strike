58d169969e5664d60bf1331be0e407c617d40db5
Syakirahzd <syaasiui@gmail.com>
2025-09-24 11:29:28 +0700

    Complete: Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
 from django.utils import timezone
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
     CATEGORY_CHOICES = [
         ('jersey', 'Jersey'),
         ('balls', 'Balls'),
